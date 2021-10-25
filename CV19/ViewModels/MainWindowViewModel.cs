ebb4f8d1036f3724e841c43b8bceccd74bc821db
tampl <tampl@CASTLE>
2021-10-26 01:15:35 +1000

    Наполнение контентом

---
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using CV19.ViewModels.Base;
+
+namespace CV19.ViewModels
+{
+	internal class MainWindowViewModel : ViewModel
+	{
+		#region Title
+		private string _title = "Анализ статистики CV19";
+
+		/// <summary>
+		/// Заголовок окна
+		/// </summary>
+		public string Title
+		{
+			get => _title;
+
+			//set
+			//{
+			//	if (Equals(_title, value)) return;
+			//	_title = value;
+			//	OnPropertyChanged();
+			//}
+			set => Set(ref _title, value);
+		}
+		#endregion
+
+		#region Status
+		private string _status = "Готов!";
+
+		/// <summary>
+		/// Статус программы
+		/// </summary>
+		public string Status
+		{
+			get => _status;
+			set => Set(ref _status, value);
+		}
+
+		#endregion
+
+	}
+}
