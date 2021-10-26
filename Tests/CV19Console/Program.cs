74022f90330be7e8fb08c92b9ab4eccf4b40c1ad
tampl <tampl@CASTLE>
2021-10-27 00:45:07 +1000

    Промежуточный коммит (проверяем загрузку статистикис с сайта)

--- Tests/CV19Console/Program.cs
+++ Tests/CV19Console/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Net.Http;
+//using System.Net;
 
 namespace CV19Console
 {
 	class Program
 	{
+		private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			//var items = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+			//var last_item = items[^1];	// запись разрешенная в .Net Core
+			//var prelast_item = items[^2];
+			//Console.WriteLine($"{last_item} {prelast_item}");
+
+			//WebClient web_client = new WebClient(); // устарел
+
+			var client = new HttpClient();
+
+			var response = client.GetAsync(data_url).Result;
+			var csv_str = response.Content.ReadAsStringAsync().Result;
+
+			Console.ReadKey();
 		}
 	}
 }
