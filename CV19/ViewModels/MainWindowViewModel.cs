984fa140a2ed3914eb43d57783bfc6f3ed4f81ef
tampl <tampl@CASTLE>
2021-10-30 22:00:33 +1000

    Добавляем тестовые данные в модель представления

--- CV19/ViewModels/MainWindowViewModel.cs
+++ CV19/ViewModels/MainWindowViewModel.cs
@@ -3,13 +3,27 @@ using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Input;
 using CV19.Infrastructure.Commands;
+using CV19.Models;
 using CV19.ViewModels.Base;
 
 namespace CV19.ViewModels
 {
 	internal class MainWindowViewModel : ViewModel
 	{
+		#region TestDataPoints : IEnumerable<DataPoint> - Тестовый набор данных для визуализации графиков
+
+		private IEnumerable<DataPoint> _testDataPoints;
+
+		public IEnumerable<DataPoint> TestDataPoints 
+		{ 
+			get => _testDataPoints; 
+			set => Set(ref _testDataPoints, value); 
+		}
+
+		#endregion // TestDataPoints
+
 		#region Title
+
 		private string _title = "Анализ статистики CV19";
 
 		/// <summary>
@@ -27,9 +41,11 @@ namespace CV19.ViewModels
 			//}
 			set => Set(ref _title, value);
 		}
-		#endregion
+
+		#endregion // Title
 
 		#region Status
+
 		private string _status = "Готов!";
 
 		/// <summary>
@@ -41,11 +57,12 @@ namespace CV19.ViewModels
 			set => Set(ref _status, value);
 		}
 
-		#endregion
+		#endregion // Status
 
 		#region Commands
 
 		#region CloseApplicationCommand
+
 		public ICommand CloseApplicationCommand { get; }
 
 		private void OnCloseApplicationCommandExecuted(object p)
@@ -54,6 +71,7 @@ namespace CV19.ViewModels
 		}
 
 		private bool CanCloseApplicationCommandExecute(object p) => true;
+
 		#endregion // CloseApplicationCommand
 
 
@@ -61,7 +79,22 @@ namespace CV19.ViewModels
 
 		public MainWindowViewModel()
 		{
+			#region Commands
+
 			CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted, CanCloseApplicationCommandExecute);
+
+			#endregion
+
+			var data_points = new List<DataPoint>((int)(360 / 0.1));
+			for (var x = 0d; x < 360; x += 0.1)
+			{
+				const double to_rad = Math.PI / 180;
+				var y = Math.Sin(x * to_rad);
+
+				data_points.Add(new DataPoint { XValue = x, YValue = y });
+			}
+
+			TestDataPoints = data_points;
 		}
 	}
 }
