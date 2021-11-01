25057fa1731dc0b4a3a0c863c0f5f4d5ffd6b301
tampl <tampl@CASTLE>
2021-11-01 10:53:07 +1000

    Добавление Заметки.

--- CV19/Models/Decanat/Group.cs
+++ CV19/Models/Decanat/Group.cs
@@ -7,5 +7,7 @@ namespace CV19.Models.Decanat
         public string Name { get; set; }
 
         public ICollection<Student> Students { get; set; }
+
+		public string Description { get; set; }
 	}
 }
