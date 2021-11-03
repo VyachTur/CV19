946f994f9ced3b71c1a502cb4c8db4adca9dce8e
tampl <tampl@CASTLE>
2021-11-04 00:06:30 +1000

    Добавляем работу с файловой системой (для тестирования HierarhicalDataTemplate)

---
+++ CV19/ViewModels/DirectoryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CV19.ViewModels
+{
+	class DirectoryViewModel
+	{
+		private readonly DirectoryInfo _dirInfo;
+
+		public DirectoryViewModel(string path) => _dirInfo = new DirectoryInfo(path);
+	}
+
+	class FileViewModel
+	{
+		private readonly FileInfo _fInfo;
+
+		public FileViewModel(string path) => _fInfo = new FileInfo(path);
+	}
+}
