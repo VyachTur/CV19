68b8238d4d5a23eba2b02eee59704e5268fc3da6
tampl <tampl@CASTLE>
2021-11-07 19:45:05 +1000

    Доделана CountriesStatisticViewModel

--- CV19/ViewModels/CountriesStatisticViewModel.cs
+++ CV19/ViewModels/CountriesStatisticViewModel.cs
@@ -1,19 +1,58 @@
-﻿using CV19.ViewModels.Base;
+﻿using CV19.Infrastructure.Commands;
+using CV19.Models;
+using CV19.Services;
+using CV19.ViewModels.Base;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace CV19.ViewModels
 {
 	internal class CountriesStatisticViewModel : ViewModel
 	{
-		private readonly MainWindowViewModel _mainViewModel;
+		#region Fields
+
+		private DataService _dataService;
+
+		private IEnumerable<CountryInfo> _countries;
+
+		#endregion // Fields
+
+		#region Properties
+
+		private MainWindowViewModel MainVM { get; }
+
+		public IEnumerable<CountryInfo> Countries
+		{
+			get => _countries;
+			private set => Set(ref _countries, value);
+		}
+
+		#endregion // Properties
+
+		#region Commands
+
+		public ICommand RefreshDataCommand { get; }
+
+		private void OnRefreshDataCommand(object p)
+		{
+			Countries = _dataService.GetData();
+		}
+
+		#endregion // Commands
+
 
 		public CountriesStatisticViewModel(MainWindowViewModel viewModel)
 		{
-			_mainViewModel = viewModel;
+			MainVM = viewModel;
+
+			_dataService = new DataService();
+
+			RefreshDataCommand = new LambdaCommand(OnRefreshDataCommand);
+
 		}
 	}
 }
