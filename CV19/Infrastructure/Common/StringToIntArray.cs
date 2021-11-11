5870fa20836cf5eda6c752b199d47c03e4df80aa
tampl <tampl@CASTLE>
2021-11-11 15:52:47 +1000

    Добавление MarkupExtension

---
+++ CV19/Infrastructure/Common/StringToIntArray.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Markup;
+
+namespace CV19.Infrastructure.Common
+{
+	internal class StringToIntArray
+	{
+		[ConstructorArgument("Str")]
+		public string Str { get; set; }
+
+		public StringToIntArray() { }
+
+		public StringToIntArray(string str) => Str = str;
+	}
+}
