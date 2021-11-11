f333aab2cc8852e8bd24c24a19e039b892fb87d9
tampl <tampl@CASTLE>
2021-11-11 12:37:58 +1000

    Реализация MultiBinding и MultiConverter

---
+++ CV19/Infrastructure/Converters/ToListConverter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace CV19.Infrastructure.Converters
+{
+	internal class ToListConverter : MultiConverter
+	{
+		public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture) => values.ToList();
+	}
+}
