2d8f692ac3289e641df2d1798d5f9ed04ace4974
tampl <tampl@CASTLE>
2021-11-14 17:37:30 +1000

    Рефакторинг

--- CV19/ViewModels/CountriesStatisticViewModel.cs
+++ CV19/ViewModels/CountriesStatisticViewModel.cs
@@ -8,8 +8,6 @@ using OxyPlot.Series;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
