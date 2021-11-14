aa83fccae9b5fcfe365c768b6cccd804b3e72d8e
tampl <tampl@CASTLE>
2021-11-14 17:23:42 +1000

    Добавление карты XAML.MapControl

---
+++ CV19/Infrastructure/Converters/PointToMapLocation.cs
@@ -0,0 +1,28 @@
+﻿using MapControl;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace CV19.Infrastructure.Converters
+{
+    [MarkupExtensionReturnType(typeof(PointToMapLocation))]
+    [ValueConversion(typeof(Point), typeof(Location))]
+    internal class PointToMapLocation : Converter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Point p)) return null;
+                
+            return new Location(p.X, p.Y);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Location l)) return null;
+
+            return new Point(l.Latitude, l.Longitude);
+        }
+    }
+}
