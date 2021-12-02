eafa2b6105bb36d99683b4fc006d0c3b8e920bfb
tampl <tampl@CASTLE>
2021-12-02 21:24:59 +1000

    Рефакторинг

---
+++ Tests/CV19WPFTest/Behaviors/DragInCanvas.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CV19WPFTest.Behaviors
+{
+    internal class DragInCanvas : Behavior<UIElement>
+    {
+        private Point _startPoint;
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.MouseLeftButtonDown += OnLeftButtonDown;
+        }
+
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.MouseLeftButtonDown -= OnLeftButtonDown;
+            AssociatedObject.MouseUp -= OnMouseUp;
+            AssociatedObject.MouseMove -= OnMouseMove;
+        }
+
+
+        private void OnLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            _startPoint = e.GetPosition(AssociatedObject);
+            AssociatedObject.CaptureMouse();
+            AssociatedObject.MouseMove += OnMouseMove;
+            AssociatedObject.MouseUp += OnMouseUp;
+        }
+
+
+        private void OnMouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnMouseUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            AssociatedObject.MouseUp -= OnMouseUp;
+            AssociatedObject.MouseMove -= OnMouseMove;
+            AssociatedObject.ReleaseMouseCapture();
+        }
+    }
+}
